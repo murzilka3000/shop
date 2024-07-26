61c647bc22e8edda2ba9d9e9781be090eba025da
Nikolay <nikolayzhidkov@icloud.com>
2024-07-26 03:49:53 +0300

    mixins

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/pages/home/Home"
 import Header from "./components/ui/header/Header"
 import Footer from "./components/ui/footer/Footer"
-import About from "./components/pages/about/About"
-import Cabinet from "./components/pages/cabinet/Cabinet"
-import Contacts from "./components/pages/contacts/Contacts"
 
 
 const App = () => {
@@ -13,9 +10,6 @@ const App = () => {
     <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/cabinet" element={<Cabinet/>}/>
-        <Route path="/contacts" element={<Contacts/>}/>
       </Routes>
     <Footer/>
     </BrowserRouter>
--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -3,19 +3,23 @@ import s from './Header.module.scss'
 const Header = () => {
   return (
     <header className={s.header}>
-      <div>
-        <img src="/Menu.svg" alt="menu" />
-        <span>Меню</span>
-      </div>
-      <div>
-        <img src="/Rostelecom.svg" alt="logo" />
-      </div>
-      <div>
-        <img src="/Search.svg" alt="" />
-        <img src="/Heart.svg" alt="" />
-        <img src="/Compare add.svg" alt="" />
-        <img src="/Cart.svg" alt="" />
-        <img src="/Profile.svg" alt="" />
+      <div className="wrapper">
+        <div className={s.container}>
+          <div className={s.blockOne}>
+            <img src="/Menu.svg" alt="menu" />
+            <span>Меню</span>
+          </div>
+          <div>
+            <img src="/Rostelecom.svg" alt="logo" />
+          </div>
+          <div>
+            <img src="/Search.svg" alt="" />
+            <img src="/Heart.svg" alt="" />
+            <img src="/Compare add.svg" alt="" />
+            <img src="/Cart.svg" alt="" />
+            <img src="/Profile.svg" alt="" />
+          </div>
+        </div>
       </div>
     </header>
   )
